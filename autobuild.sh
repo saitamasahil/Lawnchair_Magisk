c9c4fea53a6062c827dfdc10a232a0792cccd4c3
Saitama <sahilmanchanda1996@gmail.com>
2023-04-09 10:31:23 +0530

    improvements while downloadinf required package

--- autobuild.sh
+++ autobuild.sh
@@ -1,63 +1,57 @@
 #!/usr/bin/bash
 
-# Check for the Distro Type
+# Check for the Distro Type & Install necessary packages
 
-# Check if pkg package manager is available
-which "pkg"
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-    # Install zip and figlet package using pkg
-    pkg install zip figlet
+PACKAGE_MANAGERS=("pkg" "apt" "yum" "dnf" "pacman" "zypper")
+PACKAGES=("zip")
+
+# Define a function to check if a package is installed
+check_package() {
+  if command -v "$1" >/dev/null; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Loop through the packages and check if they are installed
+for PKG in "${PACKAGES[@]}"; do
+  check_package "$PKG"
+  # If not installed, add it to a list of packages to install
+  if [ $? -ne 0 ]; then
+    TO_INSTALL+=("$PKG")
+  fi
+done
+
+# If there are no packages to install then just print all packages are already installed
+if [ ${#TO_INSTALL[@]} -eq 0 ]; then
+  echo -e "\033[38;5;2mAll required packages are already installed.\033[0m"
+  echo -e "\033[38;5;2mStarting Lawnchair Magisk Builder...\033[0m"
 fi
 
-# Check if apt package manager is available
-which "apt"
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-    # Install zip and figlet package using apt-get
-    sudo apt-get install zip figlet
-fi
+# Loop through the package managers and find the one that is available
+for PM in "${PACKAGE_MANAGERS[@]}"; do
+  if command -v "$PM" >/dev/null; then
+    # Use the appropriate command to install the packages
+    case "$PM" in
+    "pkg") pkg install "${TO_INSTALL[@]}" ;;
+    "apt") sudo apt-get install "${TO_INSTALL[@]}" ;;
+    "yum") sudo yum install "${TO_INSTALL[@]}" ;;
+    "dnf") sudo dnf install "${TO_INSTALL[@]}" ;;
+    "pacman") sudo pacman -S --needed "${TO_INSTALL[@]}" ;;
+    "zypper") sudo zypper install "${TO_INSTALL[@]}" ;;
+    esac
+    break
+  fi
+done
 
-# Check if yum package manager is available
-which "yum"
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-    # Install zip and figlet package using yum
-    sudo yum install zip figlet
-fi
-
-# Check if dnf package manager is available
-which "dnf"
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-    # Install zip and figlet package using dnf
-    sudo dnf install zip figlet
-fi
-
-# Check if pacman package manager is available
-which "pacman"
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-    # Install zip and figlet package using pacman
-    sudo pacman -S zip figlet
-fi
-
-# Check if zypper package manager is available
-which "zypper"
-RESULT=$?
-if [ $RESULT -eq 0 ]; then
-    # Install zip and figlet package using zypper
-    sudo zypper install zip figlet
-fi
-
-# Check if the current Linux distribution is Fedora
+# If the distribution is Fedora, use dnf to install the packages and reinstall figlet
 if [ -f /etc/fedora-release ]; then
-    # Install zip and figlet package using dnf
-    sudo dnf install zip figlet
+  sudo dnf install "${TO_INSTALL[@]}"
 fi
 
-# Display "Lawnchair Magisk" in bigger fonts
-figlet "Lawnchair Magisk"
+# Display "Lawnchair Magisk Builder" in color
+echo -e "\033[38;5;208mLawnchair Magisk Builder\033[0m"
 
 # Check if zip is installed
 if ! command -v zip >/dev/null; then
